c76b603091d01f4be9383e6c41550fb76dede5aa
hamza <hamzakinectro@gmail.com>
2022-08-05 19:09:58 +0200

    add the margin in scan product

--- src/containers/ARPage/Controller.tsx
+++ src/containers/ARPage/Controller.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { Grid, Fade, IconButton, Collapse, } from '@mui/material';
-import { makeStyles, createStyles } from '@mui/styles';
-import { ProductInfoIcon, ReCenterIcon, AppArrowUpIcon, AppCameraSquareIcon } from 'src/components/icons';
-import { useTranslation } from 'react-i18next';
-import { AppGrid, AppButton } from 'src/components';
-import { useAppContext } from 'src/core/store';
-import { useHistory } from 'react-router-dom';
-import { useQueryClient } from 'react-query';
-import { QueryKeys } from 'src/core/declarations/enum';
+import React from "react";
+import { Grid, Fade, IconButton, Collapse } from "@mui/material";
+import { makeStyles, createStyles } from "@mui/styles";
+import {
+  ProductInfoIcon,
+  ReCenterIcon,
+  AppArrowUpIcon,
+  AppCameraSquareIcon,
+} from "src/components/icons";
+import { useTranslation } from "react-i18next";
+import { AppGrid, AppButton } from "src/components";
+import { useAppContext } from "src/core/store";
+import { useHistory } from "react-router-dom";
+import { useQueryClient } from "react-query";
+import { QueryKeys } from "src/core/declarations/enum";
 
 interface ARPageControllerProps {
   showControl: boolean;
@@ -19,20 +24,30 @@ interface ARPageControllerProps {
   onCompare?: Function;
 }
 
-const useStyles = makeStyles(() => createStyles({
-  topRadius: {
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
-  },
-  zIndex: {
-    zIndex: 9
-  }
-}));
+const useStyles = makeStyles(() =>
+  createStyles({
+    topRadius: {
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      borderBottomLeftRadius: 0,
+      borderBottomRightRadius: 0,
+    },
+    zIndex: {
+      zIndex: 9,
+    },
+  })
+);
 
 const ARPageController = (props: ARPageControllerProps) => {
-  const { showControl, onShowControl, showGrandControl, onInfo, onRecenter, onReview, onCompare } = props;
+  const {
+    showControl,
+    onShowControl,
+    showGrandControl,
+    onInfo,
+    onRecenter,
+    onReview,
+    onCompare,
+  } = props;
   const classes = useStyles();
   const { t } = useTranslation();
   const { productClaimToggleEvent, appLoadingStateEvent } = useAppContext();
@@ -43,172 +58,213 @@ const ARPageController = (props: ARPageControllerProps) => {
     if (onShowControl) {
       onShowControl(shouldControlDisplay);
     }
-  }
+  };
 
   const infoButtonHandle = () => {
     if (onInfo) {
       onInfo();
     }
-  }
+  };
 
   const reCenterButtonHandle = () => {
     if (onRecenter) {
       onRecenter();
     }
-  }
+  };
 
   const scanButtonHandle = () => {
     queryClient.removeQueries(QueryKeys.product, { exact: true });
     queryClient.removeQueries(QueryKeys.compareProduct, { exact: true });
     queryClient.removeQueries(QueryKeys.productComments, { exact: true });
     queryClient.removeQueries(QueryKeys.compareProducts, { exact: true });
-    queryClient.removeQueries(QueryKeys.buttonAnimationContent, { exact: true });
+    queryClient.removeQueries(QueryKeys.buttonAnimationContent, {
+      exact: true,
+    });
     queryClient.removeQueries(QueryKeys.productfinder, { exact: true });
     queryClient.removeQueries(QueryKeys.imageTargetsCodes, { exact: true });
     appLoadingStateEvent.next(true);
-    history.push('/scan-page');
-  }
+    history.push("/scan-page");
+  };
 
   const reviewButtonHandle = () => {
     if (onReview) {
       onReview();
     }
-  }
+  };
 
   const compareButtonHandle = () => {
     if (onCompare) onCompare();
     if (productClaimToggleEvent) productClaimToggleEvent.next(false);
-  }
+  };
 
   return (
     <Collapse in={showGrandControl}>
-      <Grid sx={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        marginBottom: '350px',
-        display:'grid',
-        justifyContent:"flex-end",
-        marginRight:'50px'
-      }}>
-        <Grid  style ={{color:"black"}}sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          p: '0px 20px 10px',
-        }}>
+      <Grid
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          marginBottom: "350px",
+          display: "grid",
+          justifyContent: "flex-end",
+          marginRight: "50px",
+        }}
+      >
+        <Grid
+          style={{ color: "black" }}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            p: "0px 20px 10px",
+          }}
+        >
           <AppButton
-              variant='contained'
-              style={{borderRadius:'0',
-              background:'#f93a6b'
-              }}
-              onClick={() => console.log("+++++++++++++===========1")}
-          >VR</AppButton>
+            variant="contained"
+            style={{ borderRadius: "0", background: "#f93a6b" }}
+            onClick={() => console.log("+++++++++++++===========1")}
+          >
+            VR
+          </AppButton>
         </Grid>
-        <Grid  style ={{color:"black"}}sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          p: '0px 20px 10px',
-        }}>    <AppButton
-            variant='contained'
-            style={{borderRadius:'0',
-              background:'#f93a6b'
-            }}
+        <Grid
+          style={{ color: "black" }}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            p: "0px 20px 10px",
+          }}
+        >
+          {" "}
+          <AppButton
+            variant="contained"
+            style={{ borderRadius: "0", background: "#f93a6b" }}
             onClick={() => console.log("+++++++++++++===========1")}
-        >Call</AppButton></Grid>
-        <Grid  style ={{color:"black"}}sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          p: '0px 20px 10px',
-        }}>    <AppButton
-            variant='contained'
-            style={{borderRadius:'0',
-              background:'#f93a6b'
-            }}
+          >
+            Call
+          </AppButton>
+        </Grid>
+        <Grid
+          style={{ color: "black" }}
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            p: "0px 20px 10px",
+          }}
+        >
+          {" "}
+          <AppButton
+            variant="contained"
+            style={{ borderRadius: "0", background: "#f93a6b" }}
             onClick={() => console.log("+++++++++++++===========1")}
-        >Email</AppButton></Grid>
+          >
+            Email
+          </AppButton>
+        </Grid>
       </Grid>
 
-
-      <Grid sx={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        marginBottom: '20px'
-      }}>
-        <Grid sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          p: '0px 20px 10px',
-        }}>
-          <IconButton sx={{ p: 0, zIndex: 2 }} onClick={() => infoButtonHandle()}>
+      <Grid
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          marginBottom: "20px",
+        }}
+      >
+        <Grid
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            p: "0px 20px 10px",
+          }}
+        >
+          <IconButton
+            sx={{ p: 0, zIndex: 2 }}
+            onClick={() => infoButtonHandle()}
+          >
             <ProductInfoIcon sx={{ fontSize: 42 }} />
           </IconButton>
 
-          <IconButton sx={{ p: 0, zIndex: 2 }} onClick={() => reCenterButtonHandle()}>
+          <IconButton
+            sx={{ p: 0, zIndex: 2 }}
+            onClick={() => reCenterButtonHandle()}
+          >
             <ReCenterIcon sx={{ fontSize: 42 }} />
           </IconButton>
 
-          <Grid sx={{
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            zIndex: 1
-          }}>
+          <Grid
+            sx={{
+              position: "absolute",
+              bottom: 0,
+              left: 0,
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              zIndex: 1,
+            }}
+          >
             <AppButton
               className={classes.topRadius}
               sx={{
-                width: 94,
-                height: 52,
-                backgroundColor: '#fff',
-                '&:hover': {
-                  backgroundColor: '#fff',
-                }
-              }} onClick={() => controlHandle(true)}>
+                width: 90,
+                height: 48,
+                marginBottom: "4%",
+                backgroundColor: "#fff",
+                "&:hover": {
+                  backgroundColor: "#fff",
+                },
+              }}
+              onClick={() => controlHandle(true)}
+            >
               {/*<AppArrowUpIcon sx={theme => ({ ...theme.arPageStyles.pageController.toggleArrowUp })} />*/}
               <AppButton
-                  sx={theme => ({
-                    backgroundColor: 'transparent',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'cover',
-                    borderRadius: 0,
-                    whiteSpace: 'pre-wrap',
-                    width: 65,
-                    height: 70,
-                    textAlign: 'center',
-                    position: 'relative',
-                    fontSize:'12px',
-                    ...(theme.arPageStyles.pageController.scanOtherProductBox)
-                  })}
-                  onClick={() => scanButtonHandle()}
-                  startIcon={<AppCameraSquareIcon sx={theme => ({
-                    position: 'absolute',
-                    width: '100%',
-                    height: '100%',
-                    top: 0,
-                    left: 0
-                  })} />}
+                sx={(theme) => ({
+                  backgroundColor: "transparent",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  borderRadius: 0,
+                  whiteSpace: "pre-wrap",
+                  width: 65,
+                  height: 70,
+                  textAlign: "center",
+                  position: "relative",
+                  fontSize: "10px",
+                  ...theme.arPageStyles.pageController.scanOtherProductBox,
+                })}
+                onClick={() => scanButtonHandle()}
+                startIcon={
+                  <AppCameraSquareIcon
+                    sx={(theme) => ({
+                      position: "absolute",
+                      width: "100%",
+                      height: "100%",
+                      top: 0,
+                      left: 0,
+                    })}
+                  />
+                }
               >
-                {t('ArPageScanOtherProductButtonText')}
+                {t("ArPageScanOtherProductButtonText")}
               </AppButton>
             </AppButton>
           </Grid>
-
         </Grid>
 
         <Collapse in={showControl} classes={{ wrapperInner: classes.zIndex }}>
-          <Grid className={classes.topRadius} sx={{
-            bgcolor: '#fff'
-          }}>
+          <Grid
+            className={classes.topRadius}
+            sx={{
+              bgcolor: "#fff",
+            }}
+          >
             <Fade in={showControl}>
-              <AppGrid sx={{
-                gridTemplateColumns: '1fr auto 1fr',
-                p: '24px',
-              }}>
+              <AppGrid
+                sx={{
+                  gridTemplateColumns: "1fr auto 1fr",
+                  p: "24px",
+                }}
+              >
                 <Grid>
                   {/*<AppButton*/}
                   {/*  sx={theme => ({*/}
@@ -236,28 +292,42 @@ const ARPageController = (props: ARPageControllerProps) => {
                   {/*</AppButton>*/}
                 </Grid>
                 <Grid>
-                  <IconButton sx={{
-                    transform: 'translateY(-20px)',
-                  }} onClick={() => { controlHandle(false) }}>
-                    <AppArrowUpIcon sx={theme => ({ ...theme.arPageStyles?.pageController.toggleArrowDown })
-                    } />
+                  <IconButton
+                    sx={{
+                      transform: "translateY(-20px)",
+                    }}
+                    onClick={() => {
+                      controlHandle(false);
+                    }}
+                  >
+                    <AppArrowUpIcon
+                      sx={(theme) => ({
+                        ...theme.arPageStyles?.pageController.toggleArrowDown,
+                      })}
+                    />
                   </IconButton>
                 </Grid>
-                <AppGrid sx={{
-                  rowGap: '10px'
-                }}>
+                <AppGrid
+                  sx={{
+                    rowGap: "10px",
+                  }}
+                >
                   <AppButton
                     fullWidth
-                    variant='contained'
-                    sx={theme => theme.arPageStyles?.pageController.button}
+                    variant="contained"
+                    sx={(theme) => theme.arPageStyles?.pageController.button}
                     onClick={() => reviewButtonHandle()}
-                  >{t('ArPageReviewsButtonText')}</AppButton>
+                  >
+                    {t("ArPageReviewsButtonText")}
+                  </AppButton>
                   <AppButton
                     fullWidth
-                    variant='contained'
-                    sx={theme => theme.arPageStyles?.pageController.button}
+                    variant="contained"
+                    sx={(theme) => theme.arPageStyles?.pageController.button}
                     onClick={() => compareButtonHandle()}
-                  >{t('ArPageCompareButtonText')}</AppButton>
+                  >
+                    {t("ArPageCompareButtonText")}
+                  </AppButton>
                 </AppGrid>
               </AppGrid>
             </Fade>
@@ -265,7 +335,7 @@ const ARPageController = (props: ARPageControllerProps) => {
         </Collapse>
       </Grid>
     </Collapse>
-  )
-}
+  );
+};
 
 export default ARPageController;
