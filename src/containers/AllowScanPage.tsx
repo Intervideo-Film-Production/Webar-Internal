4285bba3434d1c9f5bdcc906809ff2bfdb93ddb3
trung.trananh0907 <trung.trananh0907@gmail.com>
2022-08-09 22:51:00 +0100

    adjustments

---
+++ src/containers/AllowScanPage.tsx
@@ -0,0 +1,70 @@
+
+import { Grid, Typography } from '@mui/material';
+import React from 'react';
+import Button from '@mui/material/Button';
+import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
+
+// FIXME need data
+const AllowScanPage = () => {
+	const { t } = useTranslation();
+	const history = useHistory();
+
+	return (
+		<>
+			<Grid
+				container
+				justifyContent="center"
+				alignItems="center"
+				sx={{
+					position: "relative"
+				}}
+			>
+				<img
+					src="./imgs/image-content.png"
+					alt="allow-permissions"
+				/>
+				<Grid sx={{
+					display: 'grid',
+					alignContent: 'flex-start',
+					position: 'absolute',
+					bottom: 0,
+					left: 0,
+					right: 0,
+					background: "#fff",
+					pt: 3,
+					pb: 7
+				}}>
+					<Typography variant="h3" sx={{
+						px: 1,
+						mb: 7,
+						fontSize: '2rem',
+						fontWeight: 600,
+						color: theme => theme.palette.text.secondary,
+						textAlign: 'center'
+					}}>
+						AR requires access to <br />
+						device motion sensors.
+					</Typography>
+					<Grid container columns={16} sx={{
+						display: 'flex',
+						alignContent: 'flex-start',
+						justifyContent: "space-around",
+
+					}}>
+						<Button variant="outlined" style={{ borderRadius: 0, width: "40%" }}
+							onClick={() => { history.push('/'); }}
+
+						>Cancel</Button>
+						<Button variant="contained" style={{ borderRadius: 0, width: "40%" }}
+							onClick={() => { history.push('/scan-page'); }}
+
+						>Allow</Button>
+					</Grid>
+				</Grid>
+			</Grid>
+		</>
+	)
+}
+
+export default AllowScanPage;
