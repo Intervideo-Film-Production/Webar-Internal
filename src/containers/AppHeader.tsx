dca061ed4123b4fd003cca86d6473eb64db73354
hamza <hamzakinectro@gmail.com>
2022-09-05 12:49:38 +0200

    home icon url

--- src/containers/AppHeader.tsx
+++ src/containers/AppHeader.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { AppBar, IconButton, Toolbar, Link } from '@mui/material';
-import ShareIcon from '@mui/icons-material/Share';
-import { useAppContext } from 'src/core/store';
-import { filter } from 'rxjs';
-import { useTranslation } from 'react-i18next';
+import React, { useCallback, useEffect, useState } from "react";
+import { AppBar, IconButton, Toolbar, Link } from "@mui/material";
+import ShareIcon from "@mui/icons-material/Share";
+import { useAppContext } from "src/core/store";
+import { filter } from "rxjs";
+import { useTranslation } from "react-i18next";
 
 interface AppHeaderProps {
   brandName?: string;
@@ -12,57 +12,69 @@ interface AppHeaderProps {
 
 const AppHeader = ({ brandName, logo }: AppHeaderProps) => {
   const { initialPageUrl } = useAppContext();
-  const [shareLink, setShareLink] = useState('');
+  const [shareLink, setShareLink] = useState("");
   const { t } = useTranslation();
 
   const shareLinkHandle = useCallback(() => {
     if (navigator.share && shareLink) {
-      navigator.share({
-        title: `${t("SharePopupTitle")}${shareLink}`,
-        text: `${t("SharePopupText")}${shareLink}`,
-        url: shareLink
-      }).then(() => {
-      }).catch(err => {
-
-      })
+      navigator
+        .share({
+          title: `${t("SharePopupTitle")}${shareLink}`,
+          text: `${t("SharePopupText")}${shareLink}`,
+          url: shareLink,
+        })
+        .then(() => {})
+        .catch((err) => {});
     }
   }, [shareLink, t]);
 
   useEffect(() => {
     if (!!initialPageUrl) {
       const subscription = initialPageUrl
-        .pipe(filter(link => !!link))
-        .subscribe(link => {
+        .pipe(filter((link) => !!link))
+        .subscribe((link) => {
           setShareLink(link);
-        })
+        });
 
-      return () => { subscription.unsubscribe(); }
+      return () => {
+        subscription.unsubscribe();
+      };
     }
-  }, [initialPageUrl])
+  }, [initialPageUrl]);
 
   return (
     <AppBar
       position="fixed"
-      sx={theme => ({
+      sx={(theme) => ({
         zIndex: (theme) => theme.zIndex.modal + 1,
-        ...theme.headerStyles.root
+        ...theme.headerStyles.root,
       })}
       color="secondary"
     >
-      <Toolbar sx={{ paddingLeft: "20px", "paddingRight": "20px" }}>
-        <Link sx={theme => ({ ...theme.headerStyles.logo })} href="/">
-          <img style={{ height: '100%', width: 'auto' }} src={logo} alt={brandName} />
+      <Toolbar sx={{ paddingLeft: "20px", paddingRight: "20px" }}>
+        <Link
+          sx={(theme) => ({ ...theme.headerStyles.logo })}
+          href="/webar-internal"
+        >
+          <img
+            style={{ height: "100%", width: "auto" }}
+            src={logo}
+            alt={brandName}
+          />
         </Link>
-        <IconButton onClick={() => shareLinkHandle()} sx={theme => ({
-          marginLeft: 'auto',
-          p: 0,
-          ...theme.headerStyles.iconButton
-        })}>
+        <IconButton
+          onClick={() => shareLinkHandle()}
+          sx={(theme) => ({
+            marginLeft: "auto",
+            p: 0,
+            ...theme.headerStyles.iconButton,
+          })}
+        >
           <ShareIcon sx={{ width: "100%", height: "100%" }} />
         </IconButton>
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
 export default AppHeader;
--- src/routes/routeMap.ts
+++ src/routes/routeMap.ts
@@ -1,44 +1,55 @@
-import { ARPage, HomePage, ScanPage, ProductFinder,AllowScanPage} from "../containers";
+import {
+  ARPage,
+  HomePage,
+  ScanPage,
+  ProductFinder,
+  AllowScanPage,
+  InitialPage,
+} from "../containers";
 import { IAppRoute } from "../core/declarations/app";
 /**
  * WARNING: for further development, check deployment scenario before developing nested routes
  * currently the app is deployed to a NGINX subpath thus it gets the subpath window.location to serve as BrowserRouter's basename
  * the current solution may not apply with nested routing scenario
  * see helpers.tsx / getRootSubPath function for current solution
-*/
+ */
 export const AppPages = {
-  HomePage: '/',
-  InitialPage: '/initialize',
-  LoginPage: '/login',
-  StorePage: '/store',
-  ARPage: '/ar-page',
-  ScanPage: '/scan-page',
-  ProductFinderPage: '/product-finder',
-  AllowScanPage:'/allow-scan',
+  HomePage: "/",
+  InitialPage: "/initialize",
+  LoginPage: "/login",
+  StorePage: "/store",
+  ARPage: "/ar-page",
+  ScanPage: "/scan-page",
+  ProductFinderPage: "/product-finder",
+  AllowScanPage: "/allow-scan",
 };
 
 const routeMaps: IAppRoute[] = [
   {
     path: AppPages.HomePage,
     exact: true,
-    component: HomePage
+    component: HomePage,
   },
   {
     path: AppPages.ScanPage,
-    component: ScanPage
+    component: ScanPage,
   },
   {
     path: AppPages.ARPage,
-    component: ARPage
+    component: ARPage,
   },
   {
     path: AppPages.ProductFinderPage,
-    component: ProductFinder
+    component: ProductFinder,
   },
   {
     path: AppPages.AllowScanPage,
-    component: AllowScanPage
-  }
+    component: AllowScanPage,
+  },
+  {
+    path: AppPages.InitialPage,
+    component: InitialPage,
+  },
 ];
 
 export default routeMaps;
